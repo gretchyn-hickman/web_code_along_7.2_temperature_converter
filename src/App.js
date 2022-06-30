cb30387cbe97aa82e227a7182a7c84a1d7fa4700
CRHarding <34920615+CRHarding@users.noreply.github.com>
2022-06-30 15:45:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import WeatherDisplay from "./WeatherDisplay";
+import Error from "./Error";
 
 function App() {
   const [openWeather, setOpenWeather] = useState(null);
   const [userData, setUserData] = useState({ name: "Casey Harding", age: 74, bestInstructor: true });
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const func = (pos) => {
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=ce309858c9580a2f86ac0e0f45374afb`)
+      axios.get(`https://api.openweatherma.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=4ade206763c0f24a2dcbe10b1d355375`)
         .then(res => {
           const normalizedTemp = ((9/5) * (res.data.main.temp - 273) + 32).toFixed(2);
           const weather = {
@@ -19,16 +20,18 @@ function App() {
             cityTemp: normalizedTemp
           }
           setOpenWeather(weather);
-        }).catch(err => console.error(err))
-      }
+        }).catch(err => setError("Check back soon, our engineers are working around the clock to restore service!"))
+    }
     navigator.geolocation.getCurrentPosition(func)
   }, [])
 
   return (
     <div>
-      { openWeather && <WeatherDisplay openWeather={openWeather} userData={userData} /> }
+      { error ? <Error error={error} /> : 
+            openWeather ? <WeatherDisplay openWeather={openWeather} userData={userData} /> : <p>Weather data coming soon!</p>
+      }
     </div>
-  );
+  )
 }
 
 export default App;
